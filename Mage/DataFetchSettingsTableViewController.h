7cae0113eca24fffd7c89f3785ac70ff6ae92bd0
Dan Barela <daniel.barela@gmail.com>
2014-05-01 09:21:56 -0600

    data fetch and location services settings screens

---
+++ Mage/DataFetchSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataFetchSettingsTableViewController.h
+//  Mage
+//
+//  Created by Dan Barela on 5/1/14.
+//  Copyright (c) 2014 Dan Barela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataFetchSettingsTableViewController : UITableViewController
+
+@end
