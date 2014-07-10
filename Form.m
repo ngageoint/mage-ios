d492baab55abe17f5746620234bb4f01c1b8680e
Dan Barel <daniel.barela@gmail.com>
2014-07-10 13:02:41 -0600

    pull down the icons and show them on the map

--- Form.m
+++ Form.m
@@ -9,6 +9,36 @@
 #import "Form.h"
 #import <AFNetworking.h>
 #import "HttpManager.h"
+#import "zlib.h"
+#import "ZipFile.h"
+#import "ZipReadStream.h"
+#import "ZipException.h"
+#import "FileInZipInfo.h"
+
+static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingOptions readingOptions) {
+    if ([JSONObject isKindOfClass:[NSArray class]]) {
+        NSMutableArray *mutableArray = [NSMutableArray arrayWithCapacity:[(NSArray *)JSONObject count]];
+        for (id value in (NSArray *)JSONObject) {
+            [mutableArray addObject:AFJSONObjectByRemovingKeysWithNullValues(value, readingOptions)];
+        }
+        
+        return (readingOptions & NSJSONReadingMutableContainers) ? mutableArray : [NSArray arrayWithArray:mutableArray];
+    } else if ([JSONObject isKindOfClass:[NSDictionary class]]) {
+        NSMutableDictionary *mutableDictionary = [NSMutableDictionary dictionaryWithDictionary:JSONObject];
+        for (id <NSCopying> key in [(NSDictionary *)JSONObject allKeys]) {
+            id value = [(NSDictionary *)JSONObject objectForKey:key];
+            if (!value || [value isEqual:[NSNull null]]) {
+                [mutableDictionary removeObjectForKey:key];
+            } else if ([value isKindOfClass:[NSArray class]] || [value isKindOfClass:[NSDictionary class]]) {
+                [mutableDictionary setObject:AFJSONObjectByRemovingKeysWithNullValues(value, readingOptions) forKey:key];
+            }
+        }
+        
+        return (readingOptions & NSJSONReadingMutableContainers) ? mutableDictionary : [NSDictionary dictionaryWithDictionary:mutableDictionary];
+    }
+    
+    return JSONObject;
+}
 
 @implementation Form
 
@@ -96,20 +126,85 @@
     NSURL *serverUrl = [defaults URLForKey: @"serverUrl"];
 
     NSString* formId = [defaults objectForKey: @"formId"];
-    NSString *url = [NSString stringWithFormat:@"%@/%@/%@", serverUrl, @"api/forms", formId];
+    NSString *url = [NSString stringWithFormat:@"%@/%@/%@.zip", serverUrl, @"api/forms", formId];
+    
+    NSString *stringPath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0]stringByAppendingPathComponent:@"/Form.zip"];
+    
+    NSString *folderToUnzipTo = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0]stringByAppendingPathComponent:[NSString stringWithFormat: @"/form-%@", formId]];
     
     HttpManager *http = [HttpManager singleton];
     
+    NSLog(@"url is %@", url);
+    
     NSURLRequest *request = [http.manager.requestSerializer requestWithMethod:@"GET" URLString:url parameters: nil error: nil];
-    NSOperation *operation = [http.manager HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    AFHTTPRequestOperation *operation = [http.manager HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"Form request complete");
-        NSLog(@"form is %@", responseObject);
-        [defaults setObject:responseObject forKey:@"form"];
+        
+        NSError *error = nil;
+    
+        ZipFile *unzipFile = [[ZipFile alloc] initWithFileName:stringPath mode:ZipFileModeUnzip];
+        int totalNumberOfFiles = (int)[unzipFile numFilesInZip];
+        [unzipFile goToFirstFileInZip];
+        for (int i = 0; i < totalNumberOfFiles; i++) {
+            FileInZipInfo *info = [unzipFile getCurrentFileInZipInfo];
+            NSString *name = info.name;
+            if (![name hasSuffix:@"/"]) {
+                NSString *filePath = [folderToUnzipTo stringByAppendingPathComponent:name];
+                NSString *basePath = [filePath stringByDeletingLastPathComponent];
+                if (![[NSFileManager defaultManager] createDirectoryAtPath:basePath withIntermediateDirectories:YES attributes:nil error:&error]) {
+                    [unzipFile close];
+                    
+                    //return NO;
+                }
+                
+                [[NSData data] writeToFile:filePath options:0 error:nil];
+                
+                NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:filePath];
+                ZipReadStream *read = [unzipFile readCurrentFileInZip];
+                NSUInteger count;
+                NSMutableData *data = [NSMutableData dataWithLength:2048];
+                while ((count = [read readDataWithBuffer:data])) {
+                    data.length = count;
+                    [handle writeData:data];
+                    data.length = 2048;
+                }
+                [read finishedReading];
+                [handle closeFile];
+            }
+            
+            [unzipFile goToNextFileInZip];
+        }
+        
+        [unzipFile close];
+        
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSString *jsonFilePath = [NSString stringWithFormat:@"%@/form/%@", folderToUnzipTo, @"form.json"];
+        NSLog(@"json file path %@", jsonFilePath);
+        if ( [fileManager fileExistsAtPath:jsonFilePath] && error == nil)
+        {
+            NSString *jsonString = [[NSString alloc] initWithContentsOfFile:jsonFilePath encoding:NSUTF8StringEncoding error:NULL];
+            NSError *error;
+            
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
+            json = AFJSONObjectByRemovingKeysWithNullValues(json, NSJSONReadingAllowFragments);
+
+            [defaults setObject:json forKey:@"form"];
+        }
+        
+        
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
+    
+    NSError *error = nil;
+    if (![[NSFileManager defaultManager] fileExistsAtPath:stringPath])
+        [[NSFileManager defaultManager] createDirectoryAtPath:[stringPath stringByDeletingLastPathComponent] withIntermediateDirectories:NO attributes:nil error:&error];
+    
+    [[NSFileManager defaultManager] createFileAtPath:stringPath contents:nil attributes:nil];
+    operation.responseSerializer = [AFHTTPResponseSerializer serializer];
+    operation.outputStream = [NSOutputStream outputStreamToFileAtPath:stringPath append:NO];
     return operation;
+    
 }
-
 
 @end
