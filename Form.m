e09afd1d68e54c7a2b3d0bf0861c969589907ff0
Dan Barel <daniel.barela@gmail.com>
2014-07-10 13:05:16 -0600

    code cleanup

--- Form.m
+++ Form.m
@@ -42,84 +42,6 @@ static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingO
 
 @implementation Form
 
-//- (id) populateObjectFromJson: (NSDictionary *) json {
-//    [self setRemoteId:[json objectForKey:@"id"]];
-//    [self setUserId:[json objectForKey:@"userId"]];
-//    [self setDeviceId:[json objectForKey:@"deviceId"]];
-//    
-//    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-//    [dateFormat setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
-//    NSDate *date = [dateFormat dateFromString:[json objectForKey:@"lastModified"]];
-//    [self setLastModified:date];
-//    [self setUrl:[json objectForKey:@"url"]];
-//    NSDictionary *jsonState = [json objectForKey: @"state"];
-//    NSString *stateName = [jsonState objectForKey: @"name"];
-//    State enumValue = [stateName StateEnumFromString];
-//    [self setState:[NSNumber numberWithInt:(int)enumValue]];
-//    
-//    NSArray *coordinates = [json valueForKeyPath:@"geometry.coordinates"];
-//    CLLocation *location = [[CLLocation alloc] initWithLatitude:[[coordinates objectAtIndex:1] floatValue] longitude:[[coordinates objectAtIndex:0] floatValue]];
-//    
-//    [self setGeometry:[[GeoPoint alloc] initWithLocation:location]];
-//    
-//    return self;
-//}
-
-//+ (id) observationForJson: (NSDictionary *) json inManagedObjectContext: (NSManagedObjectContext *) context {
-//    
-//    Observation *observation = [[Observation alloc] initWithEntity:[NSEntityDescription entityForName:@"Observation" inManagedObjectContext:context] insertIntoManagedObjectContext:nil];
-//    
-//    [observation populateObjectFromJson:json];
-//    
-//    return observation;
-//}
-
-//+ (void) fetchObservationsFromServerWithManagedObjectContext: (NSManagedObjectContext *) context {
-//    HttpManager *http = [HttpManager singleton];
-//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//    NSURL *serverUrl = [defaults URLForKey: @"serverUrl"];
-//    NSString *url = [NSString stringWithFormat:@"%@/%@", serverUrl, @"FeatureServer/3/features"];
-//    [http.manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSArray *features = [responseObject objectForKey:@"features"];
-//        
-//        for (id feature in features) {
-//            Observation *o = [Observation observationForJson:feature inManagedObjectContext:context];
-//            NSDictionary *properties = [feature objectForKey: @"properties"];
-//            [o setProperties:properties];
-//            
-//            NSSet *existingObservations = [context fetchObjectsForEntityName:@"Observation" withPredicate:@"(remoteId == %@)", o.remoteId];
-//            Observation *dbObs = [existingObservations anyObject];
-//            
-//            //NSLog(@"there are %d observations", existingObservations.count);
-//            int archive = [@"archive" IntFromStateEnum];
-//            // if the Observation is archived and used to exist on this device, delete it
-//            if ([o.state intValue] == archive && dbObs != nil) {
-//                [context deleteObject:dbObs];
-//                NSLog(@"Deleting observation with id: %@", o.remoteId);
-//            }
-//            // else if the observation is not archived and doesn't exist, insert it
-//            else if ([o.state intValue] != archive && dbObs == nil) {
-//                [context insertObject:o];
-//                NSLog(@"Saving new observation with id: %@", o.remoteId);
-//            }
-//            // else if the observation is not archived, and not dirty and exists, update it
-//            else if ([o.state intValue] != archive && [o.dirty boolValue]) {
-//                [dbObs populateObjectFromJson:feature];
-//                NSLog(@"Updating object with id: %@", o.remoteId);
-//            }
-//        }
-//        
-//        NSError *error = nil;
-//        if (! [context save:&error]) {
-//            NSLog(@"Error inserting Observation: %@", error);
-//        }
-//        
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        NSLog(@"Error: %@", error);
-//    }];
-//}
-//
-
 + (NSOperation *) fetchFormInUseOperation {
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
@@ -133,8 +55,6 @@ static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingO
     NSString *folderToUnzipTo = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0]stringByAppendingPathComponent:[NSString stringWithFormat: @"/form-%@", formId]];
     
     HttpManager *http = [HttpManager singleton];
-    
-    NSLog(@"url is %@", url);
     
     NSURLRequest *request = [http.manager.requestSerializer requestWithMethod:@"GET" URLString:url parameters: nil error: nil];
     AFHTTPRequestOperation *operation = [http.manager HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
@@ -179,7 +99,6 @@ static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingO
         
         NSFileManager *fileManager = [NSFileManager defaultManager];
         NSString *jsonFilePath = [NSString stringWithFormat:@"%@/form/%@", folderToUnzipTo, @"form.json"];
-        NSLog(@"json file path %@", jsonFilePath);
         if ( [fileManager fileExistsAtPath:jsonFilePath] && error == nil)
         {
             NSString *jsonString = [[NSString alloc] initWithContentsOfFile:jsonFilePath encoding:NSUTF8StringEncoding error:NULL];
