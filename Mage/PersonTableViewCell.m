c3549d1d55c9d88e3aa0c858e1e1a8e213ef84c2
William Newman <newmanw10@gmail.com>
2014-07-25 14:54:38 -0600

    People Table Fixed the people TableViewController to handle rotate

--- Mage/PersonTableViewCell.m
+++ Mage/PersonTableViewCell.m
@@ -12,36 +12,17 @@
 
 @implementation PersonTableViewCell
 
-- (void) awakeFromNib {
+- (void) layoutSubviews {
     UIView *view = [[UIView alloc] initWithFrame:self.bounds];
     CAGradientLayer *gradient = [CAGradientLayer layer];
     gradient.frame = view.bounds;
 	gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor whiteColor] CGColor], (id)[[UIColor colorWithRed:207/255.0 green:207/255.0 blue:207/255.0 alpha:51/255.0] CGColor], nil];
-
-    [self.layer insertSublayer:gradient atIndex:0];
+	
+	[self setBackgroundView:[[UIView alloc] init]];
+	[self.backgroundView.layer insertSublayer:gradient atIndex:0];
+	
+	[super layoutSubviews];
 }
-
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-	NSLog(@"in view did load");
-
-	return [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-}
-
-
-- (void)viewDidLoad {
-	NSLog(@"in view did load");
-}
-
-- (void)willTransitionToState:(UITableViewCellStateMask)state {
-	NSLog(@"in view did load");
-
-}
-
-- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation) fromInterfaceOrientation {
-    NSLog(@"didRotateFromInterfaceOrientation:%ld",fromInterfaceOrientation);
-//    [self.tableView reloadRowsAtIndexPaths:[self.tableView indexPathsForVisibleRows] withRowAnimation:UITableViewRowAnimationNone];
-}
-
 
 - (id) populateCellWithLocation:(Location *) location {	
 	User *user = location.user;
