bff34ee230bec795b5d694ed073c0585b7d4f554
Dan Barela <daniel.barela@gmail.com>
2014-09-12 14:57:32 -0600

    commented out gradient for now

--- Mage/LoginViewController.m
+++ Mage/LoginViewController.m
@@ -152,20 +152,20 @@ id<Authentication> _authentication;
 
 //  When the view reappears after logout we want to wipe the username and password fields
 - (void)viewWillAppear:(BOOL)animated {
-    NSArray *colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:82.0/255.0 green:120.0/255.0 blue:162.0/255.0 alpha:1.0] CGColor], (id)[[UIColor colorWithRed:27.0/255.0 green:64.0/255.0 blue:105.0/25.0 alpha:1.0] CGColor], nil];
-    
-    CGGradientRef gradient;
-    gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), (CFArrayRef)colors, NULL);
-    CGPoint startPoint;
-    startPoint.x = self.view.frame.size.width/2;
-    startPoint.y = self.view.frame.size.height/2;
-    UIGraphicsBeginImageContext(self.view.bounds.size);
-    CGContextDrawRadialGradient(UIGraphicsGetCurrentContext(), gradient, startPoint, 0, startPoint, 5000, 0);
-    UIImage *gradientImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    UIImageView *gradientView = [[UIImageView alloc] initWithFrame:self.view.frame];
-    gradientView.image = gradientImage;
-    [self.view insertSubview:gradientView atIndex:0];
+//    NSArray *colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:82.0/255.0 green:120.0/255.0 blue:162.0/255.0 alpha:1.0] CGColor], (id)[[UIColor colorWithRed:27.0/255.0 green:64.0/255.0 blue:105.0/25.0 alpha:1.0] CGColor], nil];
+//    
+//    CGGradientRef gradient;
+//    gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), (CFArrayRef)colors, NULL);
+//    CGPoint startPoint;
+//    startPoint.x = self.view.frame.size.width/2;
+//    startPoint.y = self.view.frame.size.height/2;
+//    UIGraphicsBeginImageContext(self.view.bounds.size);
+//    CGContextDrawRadialGradient(UIGraphicsGetCurrentContext(), gradient, startPoint, 0, startPoint, 5000, 0);
+//    UIImage *gradientImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    UIImageView *gradientView = [[UIImageView alloc] initWithFrame:self.view.frame];
+//    gradientView.image = gradientImage;
+//    [self.view insertSubview:gradientView atIndex:0];
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 	NSURL *url = [defaults URLForKey:@"serverUrl"];
 	NSString *urlText = url != nil ? [url absoluteString] : @"";
