6ed45985efe5037ab519f0a68cad707b55b903ac
Dan Barela <daniel.barela@gmail.com>
2015-02-13 11:27:26 -0700

    pop the stack from the map when a new observation or person is clicked before havigating to the new item

---
+++ Mage/PopAndGoSegue.h
@@ -0,0 +1,13 @@
+//
+//  PopAndGoSegue.h
+//  MAGE
+//
+//  Created by Dan Barela on 2/13/15.
+//  Copyright (c) 2015 National Geospatial Intelligence Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopAndGoSegue : UIStoryboardSegue
+
+@end
