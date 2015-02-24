933495b50343ada253db023adce3cfc5dbb7c42e
Dan Barela <daniel.barela@gmail.com>
2015-02-24 09:42:01 -0700

    hide the attachment row when there are no attachments

--- Mage/AttachmentEditTableViewCell.m
+++ Mage/AttachmentEditTableViewCell.m
@@ -32,7 +32,8 @@
 }
 
 - (CGFloat) getCellHeightForValue: (id) value {
-    if (value == [NSNumber numberWithInt:0]) {
+    NSNumber *number = value;
+    if ([number isEqualToNumber: [NSNumber numberWithInt:0]]) {
         return 0.0;
     }
     return self.bounds.size.height;
