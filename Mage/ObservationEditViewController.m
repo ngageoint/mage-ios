f4668cabc1219d780e6e95d8f62e3eedcba24950
William Newman <newmanw10@gmail.com>
2015-04-13 13:15:13 -0600

    Re-encode videos as mp4s

--- Mage/ObservationEditViewController.m
+++ Mage/ObservationEditViewController.m
@@ -118,22 +118,57 @@
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    
     NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
-    NSString *contentType = @"";
-    NSString *name = @"";
-    NSString *localPath = @"";
-    
     if (CFStringCompare ((__bridge CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
         NSURL *videoUrl=(NSURL*)[info objectForKey:UIImagePickerControllerMediaURL];
         NSString *moviePath = [videoUrl path];
         
         if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
             UISaveVideoAtPathToSavedPhotosAlbum (moviePath, nil, nil, nil);
+            [picker dismissViewControllerAnimated:YES completion:NULL];
+            
+            AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:videoUrl options:nil];
+            NSArray *compatiblePresets = [AVAssetExportSession exportPresetsCompatibleWithAsset:avAsset];
+            if ([compatiblePresets containsObject:AVAssetExportPresetLowQuality]) {
+                AVAssetExportSession *exportSession = [[AVAssetExportSession alloc]initWithAsset:avAsset presetName:AVAssetExportPresetLowQuality];
+                NSString *mp4Path = [[moviePath stringByDeletingPathExtension] stringByAppendingPathExtension:@"mp4"];
+
+                exportSession.outputURL = [NSURL fileURLWithPath:mp4Path];
+                exportSession.outputFileType = AVFileTypeMPEG4;
+                [exportSession exportAsynchronouslyWithCompletionHandler:^{
+                    switch ([exportSession status]) {
+                        case AVAssetExportSessionStatusFailed:
+                            NSLog(@"Export failed: %@", [[exportSession error] localizedDescription]);
+                            break;
+                        case AVAssetExportSessionStatusCancelled:
+                            NSLog(@"Export canceled");
+                            break;
+                        case AVAssetExportSessionStatusCompleted: {
+                            UISaveVideoAtPathToSavedPhotosAlbum (mp4Path, nil, nil, nil);
+                            [picker dismissViewControllerAnimated:YES completion:NULL];
+                            
+                            NSMutableDictionary *attachmentJson = [NSMutableDictionary dictionary];
+                            [attachmentJson setValue:@"video/mp4" forKey:@"contentType"];
+                            [attachmentJson setValue:mp4Path forKey:@"localPath"];
+                            [attachmentJson setValue:[mp4Path lastPathComponent] forKey:@"name"];
+                            [attachmentJson setValue:[NSNumber numberWithBool:YES] forKey:@"dirty"];
+                            
+                            Attachment *attachment = [Attachment attachmentForJson:attachmentJson inContext:self.managedObjectContext];
+                            attachment.observation = self.observation;
+                            
+                            [[NSOperationQueue mainQueue] addOperationWithBlock:^ {
+                                [self.editDataStore.editTable beginUpdates];
+                                [self.editDataStore.editTable reloadData];
+                                [self.editDataStore.editTable endUpdates];
+                            }];
+
+                        }
+                        default:
+                            break;
+                    }
+                }];
+            }
         }
-        name = [videoUrl lastPathComponent];
-        localPath = moviePath;
-        contentType = @"video/quicktime";
     } else {
         UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
         
@@ -159,25 +194,21 @@
         NSData *imageData = UIImagePNGRepresentation(chosenImage);
         BOOL success = [imageData writeToFile:fileToWriteTo atomically:NO];
         NSLog(@"successfully wrote file %d", success);
-        contentType = @"image/png";
-        name = [NSString stringWithFormat: @"MAGE_%@.png", [dateFormatter stringFromDate: [NSDate date]]];
-        localPath = fileToWriteTo;
+        
+        NSMutableDictionary *attachmentJson = [NSMutableDictionary dictionary];
+        [attachmentJson setValue:@"image/png" forKey:@"contentType"];
+        [attachmentJson setValue:fileToWriteTo forKey:@"localPath"];
+        [attachmentJson setValue:[NSString stringWithFormat: @"MAGE_%@.png", [dateFormatter stringFromDate: [NSDate date]]] forKey:@"name"];
+        [attachmentJson setValue:[NSNumber numberWithBool:YES] forKey:@"dirty"];
+        
+        Attachment *attachment = [Attachment attachmentForJson:attachmentJson inContext:self.managedObjectContext];
+        attachment.observation = self.observation;
+        
+        [self.editDataStore.editTable beginUpdates];
+        [self.editDataStore.editTable reloadData];
+        [self.editDataStore.editTable endUpdates];
+        [picker dismissViewControllerAnimated:YES completion:NULL];
     }
-    
-    NSMutableDictionary *attachmentJson = [NSMutableDictionary dictionary];
-    [attachmentJson setValue:contentType forKey:@"contentType"];
-    [attachmentJson setValue:localPath forKey:@"localPath"];
-    [attachmentJson setValue:name forKey:@"name"];
-    [attachmentJson setValue:[NSNumber numberWithBool:YES] forKey:@"dirty"];
-    
-    
-    Attachment *attachment = [Attachment attachmentForJson:attachmentJson inContext:self.managedObjectContext];
-    attachment.observation = self.observation;
-
-    [self.editDataStore.editTable beginUpdates];
-    [self.editDataStore.editTable reloadData];
-    [self.editDataStore.editTable endUpdates];
-    [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (void) recordingAvailable:(Recording *)recording {
