018375f033779bf342f9aa6e1e8a3d8eec18b21c
William Newman <newmanw10@gmail.com>
2015-07-05 07:16:09 -0600

    Re-work how observation attachment segue works
    Fix a problem with clicking an observation attachment from the news feed (iPad split view) to quickly.  When this happens the detail pane in
    the split view gets screwed up and you cannot navigate back.  Also seemed bad to segue to a view controller that was already visible on the
    screen.  3rd the video/audio from a previous attachment click would not stop playing on a new attachment click.  The split view will now check
    to see if the image viewer is already the present view of the detail nav controller.  If so the split view will just update the content instead
    of lauching another brand new seque for the same controller.  If the view is not present the split view will launch the segue.
    
    Add navigation header to imageviewer view

--- Mage/ObservationViewController.m
+++ Mage/ObservationViewController.m
@@ -177,13 +177,10 @@
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:[_observation.properties valueForKey:@"type"] style: UIBarButtonItemStyleBordered target:nil action:nil];
     
     // Make sure your segue name in storyboard is the same as this line
-    if ([[segue identifier] isEqualToString:@"viewImageSegue"])
-    {
-        // Get reference to the destination view controller
+    if ([[segue identifier] isEqualToString:@"viewImageSegue"]) {
         ImageViewerViewController *vc = [segue destinationViewController];
-        
-        // Pass any objects to the view controller here, like...
         [vc setAttachment:sender];
+        [vc setTitle:@"Attachment"];
     } else if ([[segue identifier] isEqualToString:@"observationEditSegue"]) {
         self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style: UIBarButtonItemStyleBordered target:nil action:nil];
         ObservationEditViewController *oevc = [segue destinationViewController];
