79bc7dc96135c684563748569a1b6ba3e7ccdf94
Dan Barela <daniel.barela@gmail.com>
2015-08-20 10:55:52 -0600

    added logic to the token expired code to check that the user has accepted the consent

---
+++ Mage/ConsentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConsentViewController.h
+//  MAGE
+//
+//  Created by Dan Barela on 8/20/15.
+//  Copyright (c) 2015 National Geospatial Intelligence Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsentViewController : UIViewController
+
+@end
