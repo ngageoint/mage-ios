f409cd60a2920527f11e808af30ae5e9d12e0fd9
Dan Barela <daniel.barela@gmail.com>
2015-08-24 12:04:32 -0600

    fixing the text area cell and adding in the new icons

--- Mage/ObservationTextAreaTableViewCell.m
+++ Mage/ObservationTextAreaTableViewCell.m
@@ -21,10 +21,11 @@
 
 - (CGFloat) getCellHeightForValue:(id)value {
     UIFont *cellFont = self.valueTextView.font;
-    CGSize constraintSize = CGSizeMake(280.0f, MAXFLOAT);
-    CGSize labelSize = [value sizeWithFont:cellFont constrainedToSize:constraintSize lineBreakMode:NSLineBreakByWordWrapping];
-    [self.valueTextView setFrame:CGRectMake(self.valueTextView.frame.origin.x, self.valueTextView.frame.origin.y, labelSize.width, labelSize.height)];
-    return 55 + labelSize.height;
+    CGSize constraintSize = CGSizeMake(self.valueTextView.textContainer.size.width, MAXFLOAT);
+    CGSize labelSize = [value sizeWithFont:cellFont constrainedToSize:constraintSize lineBreakMode:NSLineBreakByCharWrapping];
+    [self.valueTextView setFrame:CGRectMake(self.valueTextView.frame.origin.x, self.valueTextView.frame.origin.y, self.valueTextView.frame.size.width, labelSize.height)];
+    
+    return self.valueTextView.textContainerInset.top + self.valueTextView.textContainerInset.bottom + self.valueTextView.frame.origin.y + labelSize.height+2 + 11;
 }
 
 @end
