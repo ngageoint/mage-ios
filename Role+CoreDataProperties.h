salamlam/mage-ios-sdk

Added CoreData role entity
* Xcode also has a new 'standard' in which entities are created.  Reworked old generated
  entities to match the new standard.

Role+CoreDataProperties.h
@@ -1,0 +1,33 @@
+//
+//  Role+CoreDataProperties.h
+//  mage-ios-sdk
+//
+//  Created by William Newman on 4/18/16.
+//  Copyright © 2016 National Geospatial-Intelligence Agency. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Role.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Role (CoreDataProperties)
+
+@property (nullable, nonatomic, retain) id permissions;
+@property (nullable, nonatomic, retain) NSString *remoteId;
+@property (nullable, nonatomic, retain) NSSet<User *> *user;
+
+@end
+
+@interface Role (CoreDataGeneratedAccessors)
+
+- (void)addUserObject:(User *)value;
+- (void)removeUserObject:(User *)value;
+- (void)addUser:(NSSet<User *> *)values;
+- (void)removeUser:(NSSet<User *> *)values;
+
+@end
+
+NS_ASSUME_NONNULL_END

ngageoint/mage-ios

Role+CoreDataProperties.h
@@ -1,0 +1,33 @@
+//
+//  Role+CoreDataProperties.h
+//  mage-ios-sdk
+//
+//  Created by William Newman on 4/19/16.
+//  Copyright © 2016 National Geospatial-Intelligence Agency. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Role.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Role (CoreDataProperties)
+
+@property (nullable, nonatomic, retain) id permissions;
+@property (nullable, nonatomic, retain) NSString *remoteId;
+@property (nullable, nonatomic, retain) NSSet<User *> *users;
+
+@end
+
+@interface Role (CoreDataGeneratedAccessors)
+
+- (void)addUsersObject:(User *)value;
+- (void)removeUsersObject:(User *)value;
+- (void)addUsers:(NSSet<User *> *)values;
+- (void)removeUsers:(NSSet<User *> *)values;
+
+@end
+
+NS_ASSUME_NONNULL_END