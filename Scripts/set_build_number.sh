2f5f89703d4580e64ca064964b0e672e5ab0918a
William Newman <newmanw10@gmail.com>
2016-06-15 08:46:29 -0600

    Prepare for 1.1.0 release
    Updated podfile
    Using git hash as build number

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -4,8 +4,8 @@
 
 #!/bin/bash
 
-#branch=${1:-'master'}
-#buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
-#echo "Updating build number to $buildNumber using branch '$branch'."
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist"
+branch=${1:-'master'}
+buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
+echo "Updating build number to $buildNumber using branch '$branch'."
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist"
