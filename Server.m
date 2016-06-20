02a15220598fb61ef9c1f2c4106b372b615174ff
William Newman <newmanw10@gmail.com>
2016-06-20 14:15:45 -0600

    Use MagicalRecord to get Server entity

--- Server.m
+++ Server.m
@@ -27,54 +27,29 @@
 }
 
 +(id) getPropertyForKey:(NSString *) key {
-    NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext];
-    
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    [request setEntity:[NSEntityDescription entityForName:@"Server" inManagedObjectContext:context]];
-    request.fetchLimit = 1;
-    
-    NSError *error;
-    NSArray *servers = [context executeFetchRequest:request error:&error];
-    
-    if (error) {
-        NSLog(@"Error getting server properties from database");
-        return nil;
-    }
+    Server *server = [Server MR_findFirst];
     
     id property = nil;
-    if (servers.count == 1) {
-        Server *server = [servers objectAtIndex:0];
-        if (server) {
-            NSDictionary *properties = server.properties;
-            property = [properties objectForKey:key];
-        }
+    if (server) {
+        NSDictionary *properties = server.properties;
+        property = [properties objectForKey:key];
     }
     
     return property;
 }
 
-+(void) setProperty:(id) property forKey:(NSString *)key {
-    NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext];
-    
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    [request setEntity:[NSEntityDescription entityForName:@"Server" inManagedObjectContext:context]];
-    request.fetchLimit = 1;
-    
-    NSArray *servers = [context executeFetchRequest:request error:nil];
-    if (servers.count != 1) {
-        Server *server = [NSEntityDescription insertNewObjectForEntityForName:@"Server" inManagedObjectContext:context];
-        server.properties = @{key: property};
-    } else {
-        Server *server = [servers objectAtIndex:0];
-        NSMutableDictionary *properties = [server.properties mutableCopy];
-        [properties setObject:property forKey:key];
-        server.properties = properties;
-    }
-    
-    NSError *error = nil;
-    if (! [context save:&error]) {
-        NSLog(@"Error updating server properties: %@", error);
-    }
++(void) setProperty:(id) property forKey:(NSString *) key {
+    [MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext) {
+        Server *server = [Server MR_findFirstInContext:localContext];
+        if (server) {
+            NSMutableDictionary *properties = [server.properties mutableCopy];
+            [properties setObject:property forKey:key];
+            server.properties = properties;
+        } else {
+            server = [Server MR_createEntityInContext:localContext];
+            server.properties = @{key: property};
+        }
+    }];
 }
 
 @end
