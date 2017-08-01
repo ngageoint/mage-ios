db3c82e3ffc082ffc4b3ae5108444cf6d4de6769
Dan Barela <daniel.barela@gmail.com>
2017-08-01 11:32:13 -0600

    added clear logic

--- Mage/ObservationDatePickerTableViewCell.m
+++ Mage/ObservationDatePickerTableViewCell.m
@@ -12,6 +12,7 @@
 @property (strong, nonatomic) UIDatePicker *datePicker;
 @property (strong, nonatomic) NSDate *date;
 @property (assign, nonatomic) BOOL canceled;
+@property (strong, nonatomic) NSDate *value;
 @end
 
 @implementation ObservationDatePickerTableViewCell
@@ -19,13 +20,6 @@
 - (void) populateCellWithFormField: (id) field andObservation: (Observation *) observation {
     self.date = nil;
     self.canceled = NO;
-    NSString *timestamp = [observation.properties objectForKey:(NSString *)[field objectForKey:@"name"]];
-    if ([timestamp length] > 0) {
-        self.date = [NSDate dateFromIso8601String:timestamp];
-        [self.textField setText:[self.date formattedDisplayDate]];
-    } else {
-        self.date = [[NSDate alloc] init];
-    }
     
     self.datePicker = [[UIDatePicker alloc] init];
     if (![NSDate isDisplayGMT]) {
@@ -33,7 +27,17 @@
     } else {
         self.datePicker.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
     }
-    self.datePicker.date = self.date;
+    
+    if ([[observation.properties objectForKey:(NSString *)[field objectForKey:@"name"]] length] > 0) {
+        self.value = [NSDate dateFromIso8601String: [observation.properties objectForKey:(NSString *)[field objectForKey:@"name"]]];
+        self.datePicker.date = self.value;
+    } else {
+        self.value = nil;
+        self.datePicker.date = [[NSDate alloc] init];
+    }
+    [self setTextFieldValue];
+    
+    [self.datePicker addTarget:self action:@selector(dateChanged:) forControlEvents:UIControlEventValueChanged];
     [self.keyLabel setText:[field objectForKey:@"title"]];
     
     UIBarButtonItem *doneBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneButtonPressed)];
@@ -45,9 +49,18 @@
     UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
     toolbar.items = [NSArray arrayWithObjects:cancelBarButton, flexSpace, [[UIBarButtonItem alloc] initWithCustomView:timeZoneLabel], flexSpace, doneBarButton, nil];
     self.textField.inputView = self.datePicker;
+    self.textField.clearButtonMode = UITextFieldViewModeWhileEditing;
     self.textField.inputAccessoryView = toolbar;
     [self.textField setDelegate:self];
     [self.requiredIndicator setHidden: ![[field objectForKey: @"required"] boolValue]];
+}
+
+- (void) setTextFieldValue {
+    if (self.value) {
+        [self.textField setText:[self.value formattedDisplayDate]];
+    } else {
+        [self.textField setText:nil];
+    }
 }
 
 - (BOOL) isEmpty {
@@ -59,25 +72,40 @@
 }
 
 - (void) cancelButtonPressed {
-    self.datePicker.date = self.date;
-    self.canceled = YES;
+    self.date = self.value;
+    self.datePicker.date = self.date ? self.date : [[NSDate alloc] init];
+    [self setTextFieldValue];
     [self.textField resignFirstResponder];
 }
 
 - (void) doneButtonPressed {
-    self.date = self.datePicker.date;
+    self.datePicker.date = self.date ? self.date : [[NSDate alloc] init];
     [self.textField resignFirstResponder];
 }
 
+- (void) dateChanged:(id) sender {
+    self.date = self.datePicker.date;
+    self.textField.text = [self.date formattedDisplayDate];
+}
+
+- (BOOL) textFieldShouldClear:(UITextField *)textField {
+    self.date = nil;
+    return YES;
+}
+
+- (void) textFieldDidBeginEditing:(UITextField *)textField {
+    [self dateChanged:textField];
+}
+
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-    NSString *newValue = [self.datePicker.date formattedDisplayDate];
-    if (!self.canceled) {
-        self.textField.text = newValue;
+    if (![self.value isEqualToDate:self.date]) {
+        id value = self.date ? [self.date iso8601String] : nil;
+
         if (self.delegate && [self.delegate respondsToSelector:@selector(observationField:valueChangedTo:reloadCell:)]) {
-            [self.delegate observationField:self.fieldDefinition valueChangedTo:[self.datePicker.date iso8601String] reloadCell:NO];
+            [self.delegate observationField:self.fieldDefinition valueChangedTo:value reloadCell:NO];
         }
     }
-    self.canceled = NO;
+    self.value = self.date;
 }
 
 - (void) setValid:(BOOL) valid {
