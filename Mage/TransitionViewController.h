3037df7e387d981651ea172e63b1d741640e7311
Dan Barela <daniel.barela@gmail.com>
2017-09-29 10:37:15 -0600

    ios11 changes

---
+++ Mage/TransitionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TransitionViewController.h
+//  MAGE
+//
+//  Created by Dan Barela on 9/29/17.
+//  Copyright © 2017 National Geospatial Intelligence Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransitionViewController : UIViewController
+
+@end
