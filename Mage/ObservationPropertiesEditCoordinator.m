486aa358a58981f644f5b2dad87dec5eb5b36e4c
Dan Barela <daniel.barela@gmail.com>
2017-12-07 16:02:26 -0700

    added radio support

--- Mage/ObservationPropertiesEditCoordinator.m
+++ Mage/ObservationPropertiesEditCoordinator.m
@@ -108,7 +108,7 @@
     NSNumber *formIndex = [field valueForKey:@"formIndex"];
     id name = [field valueForKey:@"name"];
     id value = self.currentEditValue = [[obsForms objectAtIndex:[formIndex integerValue]] objectForKey:name];
-    if ([[field objectForKey:@"type"] isEqualToString:@"dropdown"] || [[field objectForKey:@"type"] isEqualToString:@"multiselectdropdown"]) {
+    if ([[field objectForKey:@"type"] isEqualToString:@"dropdown"] || [[field objectForKey:@"type"] isEqualToString:@"multiselectdropdown"] || [[field objectForKey:@"type"] isEqualToString:@"radio"]) {
         SelectEditViewController *editSelect = [[SelectEditViewController alloc] initWithFieldDefinition:field andValue: value andDelegate: self];
         editSelect.title = [field valueForKey:@"title"];
         UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(fieldEditCanceled)];
