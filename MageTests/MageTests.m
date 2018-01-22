ef467072b921d5fb62a6ef0327fcfa9471b2fbea
Dan <daniel.barela@gmail.com>
2018-01-22 12:47:32 -0700

    Authentication tests (#63)
    * Added TravisCI file to build on ios11 and ios10
    * Wrote tests to begin testing the authentication coordinator

--- MageTests/MageTests.m
+++ MageTests/MageTests.m
@@ -1,32 +1,40 @@
 //
-//  MageTests.m
-//  MageTests
+//  MAGETests.m
+//  MAGETests
 //
+//  Created by Dan Barela on 1/9/18.
+//  Copyright © 2018 National Geospatial Intelligence Agency. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface MageTests : XCTestCase
+@interface MAGETests : XCTestCase
 
 @end
 
-@implementation MageTests
+@implementation MAGETests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
+    // test change
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
--- Podfile
+++ Podfile
@@ -9,10 +9,16 @@ target 'MAGE' do
     pod 'UIImage-Categories', '~> 0.0.1'
     pod 'HexColors', '~> 2.2.1'
     pod 'GoogleSignIn'
-    pod 'mage-ios-sdk', :git => 'https://github.com/ngageoint/mage-ios-sdk.git', :tag=> '2.0'
+    #pod 'mage-ios-sdk', :git => 'https://github.com/ngageoint/mage-ios-sdk.git', :tag=> '2.0'
     #pod 'mage-ios-sdk', :path => '../mage-ios-sdk'
+    pod 'mage-ios-sdk', :git => 'https://github.com/ngageoint/mage-ios-sdk.git'
     pod 'libPhoneNumber-iOS', '~> 0.8'
     pod 'tuneup_js'
     pod 'KTCenterFlowLayout'
     pod 'zxcvbn-ios'
+    target 'MAGETests' do
+        inherit! :search_paths
+        pod 'OCMock'
+        pod 'OHHTTPStubs'
+    end
 end
