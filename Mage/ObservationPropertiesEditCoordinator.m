e1921971fceaa6d311edf8824af42519968a6a36
Dan Barela <daniel.barela@gmail.com>
2018-01-25 11:54:04 -0700

    do not access forms before they are needed

--- Mage/ObservationPropertiesEditCoordinator.m
+++ Mage/ObservationPropertiesEditCoordinator.m
@@ -107,8 +107,8 @@
     NSArray *obsForms = [self.observation.properties objectForKey:@"forms"];
     NSNumber *formIndex = [field valueForKey:@"formIndex"];
     id name = [field valueForKey:@"name"];
-    id value = self.currentEditValue = [[obsForms objectAtIndex:[formIndex integerValue]] objectForKey:name];
     if ([[field objectForKey:@"type"] isEqualToString:@"dropdown"] || [[field objectForKey:@"type"] isEqualToString:@"multiselectdropdown"] || [[field objectForKey:@"type"] isEqualToString:@"radio"]) {
+        id value = self.currentEditValue = [[obsForms objectAtIndex:[formIndex integerValue]] objectForKey:name];
         SelectEditViewController *editSelect = [[SelectEditViewController alloc] initWithFieldDefinition:field andValue: value andDelegate: self];
         editSelect.title = [field valueForKey:@"title"];
         UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(fieldEditCanceled)];
