5e4e1d2489d4844e2fbf5665d67a844f7a5c2431
Dan Barela <daniel.barela@gmail.com>
2018-03-19 08:31:50 -0600

    dropdown edit cell theming

--- Mage/ObservationEditSelectTableViewCell.m
+++ Mage/ObservationEditSelectTableViewCell.m
@@ -7,6 +7,8 @@
 #import "ObservationEditSelectTableViewCell.h"
 #import "Theme+UIResponder.h"
 
+@import HexColors;
+
 @implementation ObservationEditSelectTableViewCell
 
 - (void) didMoveToSuperview {
@@ -15,44 +17,52 @@
 
 - (void) themeDidChange:(MageTheme)theme {
     self.backgroundColor = [UIColor dialog];
-    self.keyLabel.textColor = [UIColor primaryText];
+
+    self.labelField.textColor = [UIColor primaryText];
+
     self.valueField.textColor = [UIColor primaryText];
-    if (self.fieldValueValid) {
-        self.valueField.layer.borderColor = nil;
-        self.valueField.layer.borderWidth = 0;
-        self.requiredIndicator.textColor = [UIColor primaryText];
-    } else {
-        self.valueField.layer.cornerRadius = 4.0f;
-        self.valueField.layer.masksToBounds = YES;
-        self.valueField.layer.borderColor = [[UIColor redColor] CGColor];
-        self.valueField.layer.borderWidth = 1.0f;
-        self.requiredIndicator.textColor = [UIColor redColor];
-    }
+    self.valueField.selectedLineColor = [UIColor brand];
+    self.valueField.selectedTitleColor = [UIColor brand];
+    self.valueField.placeholderColor = [UIColor secondaryText];
+    self.valueField.lineColor = [UIColor secondaryText];
+    self.valueField.titleColor = [UIColor secondaryText];
+    self.valueField.errorColor = [UIColor colorWithHexString:@"F44336" alpha:.87];
+    self.valueField.iconFont = [UIFont fontWithName:@"FontAwesome" size:15];
+    self.valueField.iconText = @"\U0000f044";
+    self.valueField.iconColor = [UIColor secondaryText];
 }
 
 - (void) populateCellWithFormField: (id) field andValue: (id) value {
-    self.valueField.lineBreakMode = NSLineBreakByWordWrapping;
-    self.valueField.numberOfLines = 0;
+    self.labelField.lineBreakMode = NSLineBreakByWordWrapping;
+    self.labelField.numberOfLines = 0;
     
-    [self.keyLabel setText:[field objectForKey:@"title"]];
     self.value = value;
     
     if ([@"multiselectdropdown" isEqualToString:[self.fieldDefinition objectForKey:@"type"] ]) {
-        self.valueField.text = [self.value componentsJoinedByString:@", "];
+        if (!self.value || ((NSArray *)self.value).count == 0) {
+            self.valueField.text = nil;
+        } else {
+            self.valueField.text = @" ";
+        }
+        self.labelField.text = [self.value componentsJoinedByString:@", "];
     } else {
-        self.valueField.text = self.value;
+        if (!self.value) {
+            self.valueField.text = nil;
+        } else {
+            self.valueField.text = @" ";
+        }
+        self.labelField.text = self.value;
     }
     
-    [self.requiredIndicator setHidden: ![[field objectForKey: @"required"] boolValue]];
+    self.valueField.placeholder = ![[field objectForKey: @"required"] boolValue] ? [field objectForKey:@"title"] : [NSString stringWithFormat:@"%@ %@", [field objectForKey:@"title"], @"*"];
 }
 
 - (BOOL) isEmpty {
-    return [self.valueField.text length] == 0;
+    return [self.labelField.text length] == 0;
 }
 
 - (void) setValid:(BOOL) valid {
     [super setValid:valid];
-    [self themeDidChange:TheCurrentTheme];
 };
 
 @end
