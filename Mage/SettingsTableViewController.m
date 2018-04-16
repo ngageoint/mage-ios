36dd8395c585a3433fa55400dc5754efc9a006c6
Dan Barela <daniel.barela@gmail.com>
2018-04-16 13:56:30 -0600

    offline status cell circle text color is now always white

--- Mage/SettingsTableViewController.m
+++ Mage/SettingsTableViewController.m
@@ -218,7 +218,7 @@ static NSInteger legalSection = 6;
         UILabel *offlineLabel = [[UILabel alloc] init];
         offlineLabel.font = [UIFont systemFontOfSize:14];
         offlineLabel.textAlignment = NSTextAlignmentCenter;
-        offlineLabel.textColor = [UIColor primaryText];
+        offlineLabel.textColor = [UIColor whiteColor];
         offlineLabel.backgroundColor = [UIColor orangeColor];
         offlineLabel.text = @"!";
         [offlineLabel sizeToFit];
