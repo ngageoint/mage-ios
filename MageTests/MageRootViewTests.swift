56d0bc3f7f180698378929bcd1fe040114785e4e
Dan Barela <daniel.barela@gmail.com>
2021-02-26 07:45:58 -0700

    mage root view controller updates for scheme changes

--- MageTests/MageRootViewTests.swift
+++ MageTests/MageRootViewTests.swift
@@ -22,7 +22,7 @@ class MageRootViewTests: KIFSpec {
     override func spec() {
         
         describe("MageRootView") {
-            let recordSnapshots = true;
+            let recordSnapshots = false;
             Nimble_Snapshots.setNimbleTolerance(0.01);
             
             var controller: MageRootViewController!
