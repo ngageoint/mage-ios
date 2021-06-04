76a46f8177e9c34e80e7a2114dbbaf8fd8b76945
Dan <daniel.barela@gmail.com>
2021-06-04 13:45:17 -0600

    Bad API response fixes (#84)
    * ATS is now on
    
    * Adding tests for bad api responses
    
    Fixed tests for OHHTTP changes
    
    * Extra tests for mage server api responses

--- MageTests/MageServerTests.m
+++ MageTests/MageServerTests.m
@@ -29,7 +29,7 @@
     [super tearDown];
     NSString *domainName = [[NSBundle mainBundle] bundleIdentifier];
     [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:domainName];
-    [OHHTTPStubs removeAllStubs];
+    [HTTPStubs removeAllStubs];
 }
 
 - (void)testSetURLThatHasAlreadyBeenSetWithoutStoredPasswordOrAPIRetrievedAndNoConnectionShouldHaveNoLoginModules {
@@ -39,18 +39,18 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSError* notConnectedError = [NSError errorWithDomain:NSURLErrorDomain code:kCFURLErrorNotConnectedToInternet userInfo:nil];
-        return [OHHTTPStubsResponse responseWithError:notConnectedError];
+        return [HTTPStubsResponse responseWithError:notConnectedError];
     }];
 
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNil(localAuthenticationModule);
         XCTAssertNil(serverAuthModule);
         [responseArrived fulfill];
@@ -77,18 +77,18 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSError* notConnectedError = [NSError errorWithDomain:NSURLErrorDomain code:kCFURLErrorNotConnectedToInternet userInfo:nil];
-        return [OHHTTPStubsResponse responseWithError:notConnectedError];
+        return [HTTPStubsResponse responseWithError:notConnectedError];
     }];
     
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNil(localAuthenticationModule);
         XCTAssertNotNil(serverAuthModule);
         [responseArrived fulfill];
@@ -115,19 +115,19 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"] && [request.URL.path isEqualToString:@"/api"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSString* fixture = OHPathForFile(@"apiSuccess.json", self.class);
-        return [OHHTTPStubsResponse responseWithFileAtPath:fixture
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
                                                 statusCode:200 headers:@{@"Content-Type":@"application/json"}];
     }];
     
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNil(localAuthenticationModule);
         XCTAssertNotNil(serverAuthModule);
         [responseArrived fulfill];
@@ -159,19 +159,19 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"] && [request.URL.path isEqualToString:@"/api"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSString* fixture = OHPathForFile(@"apiSuccess.json", self.class);
-        return [OHHTTPStubsResponse responseWithFileAtPath:fixture
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
                                                 statusCode:200 headers:@{@"Content-Type":@"application/json"}];
     }];
     
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNotNil(localAuthenticationModule);
         XCTAssertNotNil(serverAuthModule);
         [responseArrived fulfill];
@@ -193,26 +193,26 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"] && [request.URL.path isEqualToString:@"/api"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSString* fixture = OHPathForFile(@"apiSuccess.json", self.class);
-        return [OHHTTPStubsResponse responseWithFileAtPath:fixture
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
                                                 statusCode:200 headers:@{@"Content-Type":@"application/json"}];
     }];
     
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNil(localAuthenticationModule);
         XCTAssertNotNil(serverAuthModule);
         [responseArrived fulfill];
     } failure:^(NSError *error) {
         // failure
         XCTFail(@"Should not have a failure");
-        NSLog(@"Failure");
+        NSLog(@"Failure %@", error);
     }];
     
     [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
@@ -227,10 +227,10 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"] && [request.URL.path isEqualToString:@"/api"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
-        OHHTTPStubsResponse *response = [[OHHTTPStubsResponse alloc] init];
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
+        HTTPStubsResponse *response = [[HTTPStubsResponse alloc] init];
         response.statusCode = 503;
         
         return response;
@@ -242,6 +242,8 @@
         XCTFail(@"Should not have a success");
     } failure:^(NSError *error) {
         // failure
+        NSLog(@"Exepected Error %@", error);
+        XCTAssertTrue([error.localizedDescription containsString:@"Failed to connect to server.  Received error Request failed: service unavailable (503)"]);
         [responseArrived fulfill];
         NSLog(@"Failure");
     }];
@@ -263,26 +265,138 @@
     
     XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
     
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [request.URL.host isEqualToString:@"mage.geointservices.io"] && [request.URL.path isEqualToString:@"/api"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
         NSString* fixture = OHPathForFile(@"apiSuccess.json", self.class);
-        return [OHHTTPStubsResponse responseWithFileAtPath:fixture
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
                                                 statusCode:200 headers:@{@"Content-Type":@"application/json"}];
     }];
     
     [MageServer serverWithURL:[NSURL URLWithString:@"https://mage.geointservices.io"] success:^(MageServer *mageServer) {
         // success
         NSLog(@"Success");
-        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:LOCAL]];
-        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:[Authentication authenticationTypeToString:SERVER]];
+        id<Authentication> localAuthenticationModule = [mageServer.authenticationModules objectForKey:@"offline"];
+        id<Authentication> serverAuthModule = [mageServer.authenticationModules objectForKey:@"local"];
         XCTAssertNotNil(localAuthenticationModule);
         XCTAssertNotNil(serverAuthModule);
         [responseArrived fulfill];
     } failure:^(NSError *error) {
         // failure
         XCTFail(@"Should not have a failure");
-        NSLog(@"Failure");
+        NSLog(@"Failure %@", error);
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void) testAPIReturnNonMageAPIJson {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:nil forKey:@"baseServerUrl"];
+    
+    XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
+    
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"magetest"] && [request.URL.path isEqualToString:@"/api"];
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
+        // just something that does not have the correct api properties
+        NSString* fixture = OHPathForFile(@"registrationSuccess.json", self.class);
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
+                                              statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+    }];
+    
+    [MageServer serverWithURL:[NSURL URLWithString:@"https://magetest"] success:^(MageServer *mageServer) {
+        XCTFail(@"Should have had a failure");
+    } failure:^(NSError *error) {
+        NSLog(@"Exepected Error %@", error);
+        XCTAssertTrue([error.localizedDescription containsString:@"Invalid server response {"]);
+        [responseArrived fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void) testAPIReturnSomethingCrazy {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:nil forKey:@"baseServerUrl"];
+    
+    XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
+    
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"magetest"] && [request.URL.path isEqualToString:@"/api"];
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
+        // just some nonsense
+        NSString* fixture = OHPathForFile(@"test_marker.png", self.class);
+        return [HTTPStubsResponse responseWithFileAtPath:fixture statusCode:200 headers:@{@"Content-Type":@"image/png"}];
+    }];
+    
+    [MageServer serverWithURL:[NSURL URLWithString:@"https://magetest"] success:^(MageServer *mageServer) {
+        XCTFail(@"Should have had a failure");
+    } failure:^(NSError *error) {
+        NSLog(@"Exepected Error %@", error);
+        XCTAssertTrue([error.localizedDescription containsString:@"Unknown API response received from server."]);
+        [responseArrived fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void) testAPIReturnNoData {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:nil forKey:@"baseServerUrl"];
+    
+    XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
+    
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"magetest"] && [request.URL.path isEqualToString:@"/api"];
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
+        HTTPStubsResponse *response = [[HTTPStubsResponse alloc] init];
+        response.statusCode = 200;
+        response.httpHeaders = @{
+            @"Content-Type": @"application/json"
+        };
+        return response;
+    }];
+    
+    [MageServer serverWithURL:[NSURL URLWithString:@"https://magetest"] success:^(MageServer *mageServer) {
+        XCTFail(@"Should have had a failure");
+    } failure:^(NSError *error) {
+        NSLog(@"Exepected Error %@", error);
+        XCTAssertEqualObjects(@"Empty API response received from server.", error.localizedDescription);
+        [responseArrived fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void) testAPIReturnHTML {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:nil forKey:@"baseServerUrl"];
+    
+    XCTestExpectation* responseArrived = [self expectationWithDescription:@"Server URL Set"];
+    
+    [HTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"magetest"] && [request.URL.path isEqualToString:@"/api"];
+    } withStubResponse:^HTTPStubsResponse*(NSURLRequest *request) {
+        NSString* fixture = OHPathForFile(@"noResponse.html", self.class);
+        return [HTTPStubsResponse responseWithFileAtPath:fixture
+                                              statusCode:200 headers:@{@"Content-Type":@"text/html"}];
+    }];
+    
+    [MageServer serverWithURL:[NSURL URLWithString:@"https://magetest"] success:^(MageServer *mageServer) {
+        XCTFail(@"Should have had a failure");
+    } failure:^(NSError *error) {
+        NSLog(@"Error %@", error);
+        XCTAssertTrue([error.localizedDescription containsString:@"Invalid API response received from server. <html>"]);
+        [responseArrived fulfill];
     }];
     
     [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
