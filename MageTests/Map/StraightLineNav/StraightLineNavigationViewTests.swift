4646fc556efcb56a117297740547434688bcbe3f
Daniel Barela <daniel.barela@gmail.com>
2024-10-25 11:24:27 -0600

    Update Straight line nav tests

--- MageTests/Map/StraightLineNav/StraightLineNavigationViewTests.swift
+++ MageTests/Map/StraightLineNav/StraightLineNavigationViewTests.swift
@@ -17,50 +17,45 @@ import MagicalRecord
 
 @testable import MAGE
 
-class StraightLineNavigationViewTests: KIFSpec {
+class StraightLineNavigationViewTests: XCTestCase {
     
-    override func spec() {
-                
-        describe("StraightLineNavigationViewTests") {
-            
-//            var straightLineNavigationView: StraightLineNavigationView!
-            
-            var view: UIView!
-            var controller: UIViewController!
-            var window: UIWindow!;
-            
-            beforeEach {
-                window = TestHelpers.getKeyWindowVisible()
-                controller = UIViewController();
-                view = controller.view;
-                view.backgroundColor = .systemGray;
-                
-                window.rootViewController = controller;
-            }
-            
-            afterEach {
-                for view in view.subviews {
-                    view.removeFromSuperview()
-                }
-                controller.dismiss(animated: false, completion: nil);
-                window.rootViewController = nil;
-                controller = nil;
-            }
-            
-            it("should load the view") {
-                let destination = CLLocationCoordinate2D(latitude: 40.1, longitude: -105.3);
-                let coordinate = CLLocationCoordinate2D(latitude: 40.008, longitude: -105.2677);
-                let location = CLLocation(coordinate: coordinate, altitude: 1625.8, horizontalAccuracy: 5.2, verticalAccuracy: 1.3, course: 200, courseAccuracy: 12.0, speed: 254.0, speedAccuracy: 15.0, timestamp: Date());
-                let mockedCLLocationManager = MockCLLocationManager();
-                mockedCLLocationManager.mockedLocation = location;
-                
-                let markerStubPath: String! = OHPathForFile("test_marker.png", StraightLineNavigationViewTests.self);
-                var straightLineNavigationView = StraightLineNavigationView(locationManager: mockedCLLocationManager, destinationMarker: UIImage(contentsOfFile: markerStubPath), destinationCoordinate: destination, delegate: nil, scheme: MAGEScheme.scheme());
-                straightLineNavigationView.populate();
-                
-                view.addSubview(straightLineNavigationView)
-                straightLineNavigationView.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .top)
-            }
+    var view: UIView!
+    var controller: UIViewController!
+    var window: UIWindow!;
+    
+    @MainActor
+    override func setUp() {
+        window = TestHelpers.getKeyWindowVisible()
+        controller = UIViewController();
+        view = controller.view;
+        view.backgroundColor = .systemGray;
+        
+        window.rootViewController = controller;
+    }
+    
+    @MainActor
+    override func tearDown() {
+        for view in view.subviews {
+            view.removeFromSuperview()
         }
+        controller.dismiss(animated: false, completion: nil);
+        window.rootViewController = nil;
+        controller = nil;
+    }
+    
+    @MainActor
+    func testShouldLoadTheView() {
+        let destination = CLLocationCoordinate2D(latitude: 40.1, longitude: -105.3);
+        let coordinate = CLLocationCoordinate2D(latitude: 40.008, longitude: -105.2677);
+        let location = CLLocation(coordinate: coordinate, altitude: 1625.8, horizontalAccuracy: 5.2, verticalAccuracy: 1.3, course: 200, courseAccuracy: 12.0, speed: 254.0, speedAccuracy: 15.0, timestamp: Date());
+        let mockedCLLocationManager = MockCLLocationManager();
+        mockedCLLocationManager.mockedLocation = location;
+        
+        let markerStubPath: String! = OHPathForFile("test_marker.png", StraightLineNavigationViewTests.self);
+        var straightLineNavigationView = StraightLineNavigationView(locationManager: mockedCLLocationManager, destinationMarker: UIImage(contentsOfFile: markerStubPath), destinationCoordinate: destination, delegate: nil, scheme: MAGEScheme.scheme());
+        straightLineNavigationView.populate();
+        
+        view.addSubview(straightLineNavigationView)
+        straightLineNavigationView.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .top)
     }
 }
