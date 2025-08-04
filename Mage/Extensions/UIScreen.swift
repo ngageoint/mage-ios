5da32ce1d6959e73c5719287736151f20e2cb6c3
jwmcdougall <james.mcdougall@omnifederal.com>
2025-08-04 16:41:29 -0500

    Intro Login Views MVP

---
+++ Mage/Extensions/UIScreen.swift
@@ -0,0 +1,9 @@
+//
+//  UIScreen.swift
+//  MAGE
+//
+//  Created by James McDougall on 7/29/25.
+//  Copyright © 2025 National Geospatial Intelligence Agency. All rights reserved.
+//
+
+import Foundation
---
+++ Mage/IntroViews/Views/IntroView.swift
@@ -0,0 +1,19 @@
+//
+//  IntroView.swift
+//  MAGE
+//
+//  Created by James McDougall on 8/4/25.
+//  Copyright © 2025 National Geospatial Intelligence Agency. All rights reserved.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
