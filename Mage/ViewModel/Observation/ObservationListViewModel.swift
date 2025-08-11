14b13a06e693337e7949417499deed267a5b2931
Brent Michalski <Brent@VegaFederal.com>
2025-08-11 14:04:29 -0500

    Got a Unit Test suite added for image/attachment pulling

--- Mage/ViewModel/Observation/ObservationListViewModel.swift
+++ Mage/ViewModel/Observation/ObservationListViewModel.swift
@@ -18,13 +18,6 @@ class ObservationListViewModel: ObservationViewViewModel, NSFetchedResultsContro
     private var observableObjectID: NSManagedObjectID
     
     // Computed property for ordered attachments
-//    var orderedAttachmentsxx: [AttachmentModel] {
-//        // Replicate the ordering logic from your previous code if needed,
-//        // or just return attachments (which are already sorted by lastModified)
-//        // You can replace this with your own sorting logic if necessary
-//        return attachments
-//    }
-    
     var orderedAttachments: [AttachmentModel]? {
         var observationForms: [[String: Any]] = []
         if let properties = observationModel?.properties as? [String: Any] {
@@ -70,51 +63,6 @@ class ObservationListViewModel: ObservationViewViewModel, NSFetchedResultsContro
         })
     }
     
-    var orderedAttachmentsFart: [AttachmentModel] {
-        var observationForms: [[String: Any]] = []
-        if let properties = observationModel?.properties as? [String: Any] {
-            if (properties.keys.contains("forms")) {
-                observationForms = properties["forms"] as! [[String: Any]];
-            }
-        }
-        
-        return attachments.sorted(by: { first, second in
-            // return true if first comes before second, false otherwise
-            
-            if first.formId == second.formId {
-                // if they are in the same form, sort on field
-                if first.fieldName == second.fieldName {
-                    // if they are the same field return the order comparison unless they are both zero, then return the lat modified comparison
-                    let firstOrder = first.order.intValue
-                    let secondOrder = second.order.intValue
-                    return (firstOrder != secondOrder) ? (firstOrder < secondOrder) : (first.lastModified ?? Date()) < (second.lastModified ?? Date())
-                } else {
-                    // return the first field
-                    let form = observationForms.first { form in
-                        return form[FormKey.id.key] as? String == first.formId
-                    }
-                    
-                    let firstFieldIndex = (form?[FormKey.fields.key] as? [[String: Any]])?.firstIndex { form in
-                        return form[FieldKey.name.key] as? String == first.fieldName
-                    } ?? 0
-                    let secondFieldIndex = (form?[FormKey.fields.key] as? [[String: Any]])?.firstIndex { form in
-                        return form[FieldKey.name.key] as? String == second.fieldName
-                    } ?? 0
-                    return firstFieldIndex < secondFieldIndex
-                }
-            } else {
-                // different forms, sort on form order
-                let firstFormIndex = observationForms.firstIndex { form in
-                    return form[FormKey.id.key] as? String == first.formId
-                } ?? 0
-                let secondFormIndex = observationForms.firstIndex { form in
-                    return form[FormKey.id.key] as? String == second.formId
-                } ?? 0
-                return firstFormIndex < secondFormIndex
-            }
-        })
-    }
-
     // Pass in observation's objectID so we can safely fetch (never switches)
     init(observationObjectID: NSManagedObjectID, context: NSManagedObjectContext? = nil) {
         self.observableObjectID = observationObjectID
