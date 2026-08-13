0d1d1f03ceadd6332fd70b0070140a3ed620eeb6
Daniel Barela <daniel.barela@gmail.com>
2026-08-13 11:31:23 -0600

    removing obsolete pull method

--- Mage/CoreData/Settings.swift
+++ Mage/CoreData/Settings.swift
@@ -25,47 +25,4 @@ extension Settings {
     @objc public static func getSettings() -> Settings? {
         return Settings.mr_findFirst()
     }
-    
-    @objc public func populate(_ json: [AnyHashable : Any]) {
-        self.mapSearchUrl = json[SettingsKey.mobileNominatimUrl.key] as? String
-        let mapSearchType = json[SettingsKey.mobileSearchType.key] as? String
-        switch (mapSearchType) {
-            case "NATIVE": self.mapSearchType = .native
-            case "NOMINATIM": self.mapSearchType = .nominatim
-            default: self.mapSearchType = .none
-        }
-    }
-    
-    @objc public static func operationToPullMapSettings(success: ((URLSessionDataTask,Any?) -> Void)?, failure: ((URLSessionDataTask?, Error) -> Void)?) -> URLSessionDataTask? {
-        guard let manager = MageSessionManager.shared(), let baseURL = MageServer.baseURL() else {
-            return nil;
-        }
-        let url = "\(baseURL)/api/settings/map";
-        
-        let task = manager.get_TASK(url, parameters: nil, progress: nil) { task, response in
-            guard let response = response as? [AnyHashable : Any] else {
-                return;
-            }
-
-            MagicalRecord.save { context in
-                var settings = Settings.mr_findFirst(in: context)
-                if (settings == nil) {
-                    settings = Settings.mr_createEntity(in: context)
-                }
-
-                settings?.populate(response)
-            } completion: { contextDidSave, error in
-                if let error = error {
-                    failure?(task, error);
-                } else {
-                    success?(task, response);
-                }
-            }
-        } failure: { task, error in
-            NSLog("Error \(error)")
-            failure?(task, error);
-        };
-
-        return task;
-    }
 }
